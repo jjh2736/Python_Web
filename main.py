7706cd87ab4791c14ccde787d7dbcb570df3e5c1
jjh2736 <79976769+jjh2736@users.noreply.github.com>
2021-03-13 22:40:59 +0900

    Jongha (#1)
    * 수정
    
    * 수정
    
    * 수정
    
    * flask html 적용완료
    
    * Delete .idea directory

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
-#플라스크 메인 페이지
 @app.route("/")
-def main():
+def hello():
     return render_template('index.html')
-#회원가입 페이지
-@app.route("/signup") 
-def signup():
-    return render_template('membership.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app.run()
