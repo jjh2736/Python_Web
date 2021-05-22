6877fbb5734d0759e77fa07c1df026eb9faf5532
jongha <jgh113114@naver.com>
2021-05-23 01:27:53 +0900

    header.html, footer.html import 완료

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+#플라스크 메인 페이지
 @app.route("/")
-def hello():
+def main():
     return render_template('index.html')
+#회원가입 페이지
+@app.route("/signup") 
+def signup():
+    return render_template('membership.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
